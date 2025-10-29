b5cdf127262a77fba41a1df7463bae1b433a3649
Lex-ico <teralex142@gmail.com>
2025-10-29 09:38:15 +0100

    arreglar programa v1

---
+++ untitled/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner uuuuuuuuuuuuuuuuuuuu = new Scanner(System.in);
+        boolean eeeee=true;
+        while (((eeeeee))) {
+            int jfdskfs = (int) (Math.random() * 100) + 1;
+            System.out.println(jfdskfs);
+            for (int jkafj=1;jksafj<100;jksafj++) {
+                int fkdlsja=uuuuuuuuuuuuuuuuuuuu.nextInt();
+                if (fkdlsja==67) System.out.println("67 67 67 67 67 67 67");
+                else if (((jfdskfs>fkdlsja) == true))                                                                                                    ;
+                else if ((fkdlsja>jfdskfs != false))                                                                                                    ;
+                else if (fkdlsja=jfdskfs) break
+                int jkfdskfjaskjs=1983298321, ijdajdwij=481298418;
+                if(jfdskfs == a) ; else {}          }
+            // eeeeee
+            iiiiiii=HOLA.nextBoolean();
+
+        }
+    }
+    public static String aa(){
+        System.out.println("wiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwiwi");
+    }
+}
